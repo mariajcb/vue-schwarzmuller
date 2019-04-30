074129efb66796444e4bd76430ed633d3a581331
Maria Bogomaz <bogomazm@gmail.com>
2019-04-30 16:02:01 -0600

    complete exercise 4

--- Exercise-4/app.js
+++ Exercise-4/app.js
@@ -1,11 +1,33 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    effect: true,
+    userClass: '',
+    isVisible: true,
+    myStyle: {
+        backgroundColor: 'purple',
+        width: '100px',
+        height: '150px'
+    },
+    progressBar: {
+      width: '0px',
+      backgroundColor: 'red'
+    }
   },
   methods: {
     startEffect: function() {
-    
+      var vm = this;
+      setInterval(function() {
+        vm.effect = !vm.effect;
+      }, 1000);
+    },
+    startProgress: function() {
+      var vm = this;
+      var width = 0;
+      setInterval(function() {
+        width = width + 10;
+        vm.progressBar.width = width + 'px';
+      }, 500)
     }
   }
 });
