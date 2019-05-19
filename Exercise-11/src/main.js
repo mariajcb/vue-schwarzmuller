3eed30301d75c1bbc9292e40445dc9eaffe6107d
Maria Bogomaz <bogomazm@gmail.com>
2019-05-19 11:23:26 -0600

    completes Exercise 11

---
+++ Exercise-11/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// Vue.filter('to-lowercase', function(value) {
+//     return value.toLowerCase();
+// });
+//
+// Vue.mixin({
+//     created() {
+//         console.log('Global Mixin - Created Hook');
+//     }
+// });
+
+Vue.filter('calculateLength', (value) => {
+    return value + ' (' + value.length + ')';
+})
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
