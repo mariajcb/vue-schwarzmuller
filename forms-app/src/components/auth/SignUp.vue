185c8fecb411549962d4617b0f7380f07250fb03
Maria Bogomaz <bogomazm@gmail.com>
2019-11-22 10:39:44 -0700

    add custom and async validation

--- forms-app/src/components/auth/SignUp.vue
+++ forms-app/src/components/auth/SignUp.vue
@@ -2,32 +2,39 @@
   <div id="signup">
     <div class="signup-form">
       <form @submit.prevent="onSubmit">
-        <div class="input">
+        <div class="input" :class="{invalid: $v.email.$error}">
           <label for="email">E-Mail</label>
           <input
                   type="email"
                   id="email"
+                  @blur="$v.email.$touch()"
                   v-model="email">
+          <p v-if="!$v.email.email">Please provide a valid email address.</p>
+          <p v-if="!$v.email.required">This field must not be empty.</p>
         </div>
-        <div class="input">
+        <div class="input" :class="{ invalid: $v.age.$error }">
           <label for="age">Your Age</label>
           <input
                   type="number"
                   id="age"
+                  @blur="$v.age.$touch()"
                   v-model.number="age">
+          <p v-if="!$v.age.minVal">You have to be at least {{ $v.age.$params.minVal.min }} years old.</p>
         </div>
-        <div class="input">
+        <div class="input" :class="{ invalid: $v.password.$error }">
           <label for="password">Password</label>
           <input
                   type="password"
                   id="password"
+                  @blur="$v.password.$touch()"
                   v-model="password">
         </div>
-        <div class="input">
+        <div class="input" :class="{ invalid: $v.password.$error }">
           <label for="confirm-password">Confirm Password</label>
           <input
                   type="password"
                   id="confirm-password"
+                  @blur="$v.confirmPassword.$touch()"
                   v-model="confirmPassword">
         </div>
         <div class="input">
@@ -46,22 +53,30 @@
             <div
                     class="input"
                     v-for="(hobbyInput, index) in hobbyInputs"
+                    :class="{invalid: $v.hobbyInputs.$each[index].$error}"
                     :key="hobbyInput.id">
               <label :for="hobbyInput.id">Hobby #{{ index }}</label>
               <input
                       type="text"
                       :id="hobbyInput.id"
+                      @blur="$v.hobbyInputs.$each[index].value.$touch()"
                       v-model="hobbyInput.value">
               <button @click="onDeleteHobby(hobbyInput.id)" type="button">X</button>
             </div>
+            <p v-if="!$v.hobbyInputs.minLength">You have to specify at least {{ $v.hobbyInputs.$params.minLength.min}} hobbies.</p>
+            <p v-if="!$v.hobbyInputs.required">Please add hobbies.</p>
           </div>
         </div>
-        <div class="input inline">
-          <input type="checkbox" id="terms" v-model="terms">
+        <div class="input inline" :class="{ invalid: $v.terms.$invalid }">
+          <input
+                      type="checkbox"
+                      id="terms"
+                      @change="$v.terms.$touch()"
+                      v-model="terms">
           <label for="terms">Accept Terms of Use</label>
         </div>
         <div class="submit">
-          <button type="submit">Submit</button>
+          <button type="submit" :disabled="$v.email.$invalid">Submit</button>
         </div>
       </form>
     </div>
@@ -69,6 +84,7 @@
 </template>
 
 <script>
+  import { required, email, numeric, minValue, minLength, sameAs, requiredUnless } from 'vuelidate/lib/validators'
   export default {
     data () {
       return {
@@ -79,6 +95,45 @@
         country: 'usa',
         hobbyInputs: [],
         terms: false
+      }
+    },
+    validations: {
+      email: {
+        required,
+        email,
+        unique: val => {
+          if (val === '') return true
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve(val !== 'test@test.com')
+            }, 1000)
+          })
+        }
+      },
+      age: {
+        required,
+        numeric,
+        minVal: minValue(18)
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      },
+      confirmPassword: {
+        sameAs: sameAs('password')
+      },
+      terms: {
+        customRequiredUnless: (value, vm) => vm.country === 'germany' ? true: value
+      },
+      hobbyInputs: {
+        required,
+        minLength: minLength(2),
+        $each: {
+          value: {
+            required,
+            minLength: minLength(5)
+          }
+        }
       }
     },
     methods: {
@@ -153,6 +208,15 @@
   .input select {
     border: 1px solid #ccc;
     font: inherit;
+  }
+
+  .input.invalid input {
+    border: 1px solid red;
+    background-color: #ffc9aa;
+  }
+
+  .input.invalid label {
+    color: red;
   }
 
   .hobbies button {
--- forms-app/src/main.js
+++ forms-app/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import Vuelidate from 'vuelidate'
 
 import router from './router'
 import store from './store'
+
+Vue.use(Vuelidate)
 
 axios.defaults.baseURL = process.env.VUE_APP_FIREBASE_DB_URL
 axios.defaults.headers.get['Accept'] = 'application/json'
